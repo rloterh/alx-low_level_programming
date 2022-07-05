4e9e35903254a1da5bed9625e0787f79dba44c52
Robert <rloterh@gmail.com>
2022-07-05 23:59:53 +0100

    Add function that prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
+
 #include "main.h"
 /**
- * function that prints every minute of the day, 24 hour clock
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
  * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
