c4630c27a0f18f832daa220e2d87a3910b7061ec
Robert <rloterh@gmail.com>
2022-08-19 21:45:13 +0100

    Add function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,48 +1,23 @@
-#include <stddef.h>
-#include "main.h"
-
 /**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
+ * binary_to_uint - convert string representation of binary number to uint
+ * @b: string to convert
  *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-
-	return (1);
-}
-
-
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
+ * Return: the converted number, or 0 if `b' is NULL or contains
+ * symbols besides 0 or 1.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	unsigned int n = 0;
 
-	if (!check_valid_string(b))
-		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
+	if (!b)
+		return (n);
+	while (*b)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
+		if (*b != '0' && *b != '1')
+			return (0);
+		n *= 2;
+		n += *b - '0';
+		++b;
 	}
-
-	return (decimal);
+	return (n);
 }
