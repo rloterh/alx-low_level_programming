9182fd2818aff2ac4d4385c3d7fa891ac72d6f5c
Robert <rloterh@gmail.com>
2022-08-22 17:22:44 +0100

    Add function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,33 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print to POSIX stdout
- * @filename: char string of files name
- * @letters: number of letters to read and print
- * Return: number of letters read and printed, or 0 if error
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the text file
+ * @letters: The number of letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print, otherwise 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rcount, wcount;
-	char *buffer;
+	ssize_t n = 0;
+	int fd, a;
+	void *buf = malloc(sizeof(char) * 2);
 
-	if (filename == NULL)
+	if (filename == NULL || buf == NULL)
 		return (0);
-
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	for (n = 0; n < (ssize_t)letters; n++)
 	{
-		free(buffer);
-		return (0);
+		a = read(fd, buf, 1);
+		if (a == 0)
+			break;
+		a = write(STDOUT_FILENO, buf, 1);
+		if (a != 1)
+			return (0);
 	}
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
-		return (0);
-
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-	if (wcount == -1 || rcount != wcount)
-		return (0);
-	free(buffer);
-
 	close(fd);
-	return (wcount);
+	free(buf);
+	return (n);
 }
