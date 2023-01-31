49593f26e8e9cf6863f36484435aa6eb82ea34f2
Robert Loterh <rloterh@gmail.com>
2023-01-31 21:31:13 +0000

    Add a script prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - "print" text
+ *
+ * Description: uses `printf` function to "print" text to screen
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
