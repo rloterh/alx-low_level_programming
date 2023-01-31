0ada675fbbb3ef4db85b5ba4cb3cccabb91a8595
Robert Loterh <rloterh@gmail.com>
2023-01-31 21:29:01 +0000

    Add a script that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - "put" text
+ *
+ * Description: uses `puts` function to "put" text to screen
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
