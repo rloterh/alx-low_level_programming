7ba62ad8e3de7a9715d6f8e967b0aad056f8596a
Robert Loterh <rloterh@gmail.com>
2023-02-05 19:28:40 +0000

    Add a function that splits a string into words

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,57 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * argstostr - function that concatenates all the arguments of your program.
- *
- *
- * @ac: ARGC
- * @av: ARGV
- *
- * Return: Pointer
- */
+*len - returns length of str
+*@str: string counted
+*Return: returns the length
+*/
+int len(char *str)
+{
+		int len = 0;
+
+		if (str != NULL)
+		{
+			while (str[len])
+				len++;
+		}
+	return (len);
+}
+
+/**
+* argstostr - a function that concatenates all the arguments of your program
+*@ac: count of args passed to the function
+*@av:array of arguments
+*
+*Return: pointer to the new string
+*/
+
 char *argstostr(int ac, char **av)
 {
-	int i = 0, j, longitud = 0, l, k;
-	char *string;
+		char *new_string = NULL;
+		int k = 0, i = ac, j, sum = 0, temp = 0;
 
-	if (ac == 0 || av == '\0')
-	return (NULL);
+		if (ac == 0 || av == NULL)
+			return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
+		while (ac--)
+			sum += (len(av[ac]) + 1);
+		new_string = (char *) malloc(sum + 1);
+
+		if (new_string != NULL)
 		{
-			j++;
+			while (k < i)
+			{
+				for (j = 0; av[k][j] != '\0'; j++)
+					new_string[j + temp] = av[k][j];
+				new_string[temp + j] = '\n';
+				temp += (j + 1);
+				k++;
+			}
+			new_string[temp] = '\0';
 		}
-		longitud  +=  j + 1;
-	}
-	string = malloc(sizeof(char) * (longitud + 1));
-
-	if (string == NULL)
-	{
-		return (NULL);
-	}
-	longitud = 0;
-	for (k = 0; k < ac; k++)
-	{
-		for (l = 0; av[k][l] != '\0'; l++)
+		else
 		{
-			*(string + longitud) = av[k][l];
-			longitud++;
+			return (NULL);
 		}
-		*(string + longitud) = '\n';
-		longitud++;
-	}
-
-	return (string);
+		return (new_string);
 }
